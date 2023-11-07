79ccbc6fca14950d3ca3c3648f02b492336849b6
harshil <harshilsuthar427@gmail.com>
2023-11-07 16:31:57 +0530

    Algorithms updated

--- lib/Screens/dashboard/HomeScreen.dart
+++ lib/Screens/dashboard/HomeScreen.dart
@@ -52,7 +52,7 @@ class _HomeScreenState extends State<HomeScreen> {
         SizedBox(
           height: 20,
         ),
-        StockList(),
+        SingleChildScrollView(child: StockList()),
       ],
     );
   }
